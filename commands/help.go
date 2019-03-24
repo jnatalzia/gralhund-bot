e7cf55ddde3f992091fee12f577fc2cc292204e7
kittensandsloths <48889759+kittensandsloths@users.noreply.github.com>
2019-03-24 14:06:27 -0400

    Update help.go

--- commands/help.go
+++ commands/help.go
@@ -9,7 +9,7 @@ var allHelpInfo = []helpInfo{
 	helpInfo{Command: "help", Description: "Displays this list."},
 	helpInfo{Command: "ping", Description: "Returns a `pong` if Gralhund is up and running."},
 	helpInfo{Command: "gif me <search_term>?", Description: "Returns a gif either random or based on the provided search term."},
-	helpInfo{Command: "make emoji from \"<image_url>\" with name \"<emoji_name>\"", Description: "Creates a gif based on the provided image link. Supports jpeg and png."},
+	helpInfo{Command: "make emoji from \"<image_url>\" with name \"<emoji_name>\"", Description: "Creates an emoji based on the provided image link. Supports jpeg and png."},
 	helpInfo{Command: "give <pts> points to @<username>", Description: "Gives points to that user."},
 	helpInfo{Command: "take <pts> points from @<username>", Description: "Takes points from that user."},
 	helpInfo{Command: "show point leaderboard", Description: "Shows top points."},
